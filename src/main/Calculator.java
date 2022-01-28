af88a2969352d3d3dd3a98f437f23564dca3156e
WellPlacedCactus <abowlasoup@gmail.com>
2022-01-27 20:32:03 -0600

    Initial

---
+++ src/main/Calculator.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Calculator {
+
+	public Calculator() {
+		
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+
+	public static void main(String args[]) {
+		Calculator c = new Calculator();
+		System.out.println(c.add(2, 3));
+	}
+}
